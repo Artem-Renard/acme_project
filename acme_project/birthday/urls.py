dfc0022cbfe1bded4b5ad7ebd3b3b1c6b36b6666
Artem Mukoseev <AVMukoseev@sberbank.ru>
2024-05-28 17:37:09 +0300

    Спринт 9/18 → Тема 2/3: Работа с формами → Урок 10/10

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,3 +1,7 @@
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -9,4 +13,4 @@ urlpatterns = [
     path('list/', views.birthday_list, name='list'),
     path('<int:pk>/edit/', views.birthday, name='edit'),
     path('<int:pk>/delete/', views.delete_birthday, name='delete'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
