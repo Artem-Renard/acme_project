b7f424e566bdec76a9cb5678c1c6d51256866bd2
Artem Mukoseev <AVMukoseev@sberbank.ru>
2024-05-31 16:17:24 +0300

    Спринт 9/18 → Тема 3/3: Представления: расширенные возможности → Урок 2/4

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -9,8 +9,8 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.birthday, name='create'),
-    path('list/', views.birthday_list, name='list'),
-    path('<int:pk>/edit/', views.birthday, name='edit'),
-    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+    path('', views.BirthdayCreateView.as_view(), name='create'),
+    path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
+    path('list/', views.BirthdayListView.as_view(), name='list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,55 +1,37 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.core.paginator import Paginator
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        # Файлы, переданные в запросе, указываются отдельно.
-        files=request.FILES or None,
-        instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
-    birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
-    paginator = Paginator(birthdays, 10)
-
-    # Получаем из запроса значение параметра page.
-    page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора.
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
-    page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст
-    # объект страницы пагинатора
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
