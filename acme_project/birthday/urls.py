1f74bc01eee70f560aff13771f128d3facdc4dd6
Artem Mukoseev <AVMukoseev@sberbank.ru>
2024-05-31 18:15:22 +0300

    Спринт 9/18 → Тема 3/3: Представления: расширенные возможности → Урок 3/4

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 app_name = 'birthday'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.BirthdayCreateView.as_view(), name='create'),
+    path('list/', views.BirthdayListView.as_view(), name='list'),
+    path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
-    path('list/', views.BirthdayListView.as_view(), name='list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
