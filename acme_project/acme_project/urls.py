a0927838334ac1013fe34761bd84bb4d5d918783
Artem Mukoseev <AVMukoseev@sberbank.ru>
2024-06-02 11:57:12 +0300

    Спринт 10/18 → Тема 1/4: Пользователи в Django → Урок 5/6

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+
+# Добавьте новые строчки с импортами классов.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+# К импортам из django.urls добавьте импорт функции reverse_lazy
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -7,4 +13,13 @@ urlpatterns = [
     path('birthday/', include('birthday.urls')),
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ]
